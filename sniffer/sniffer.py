66ade8bfde27b418e7f27f36f4e8dd1336301172
Patryk Kaiser <73563195+Telephonelines@users.noreply.github.com>
2022-10-27 19:22:30 +0100

    Update sniffer.py

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -4,5 +4,6 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
+# Simply print the packet. This will be printed in Python Bytes Object form.
 while True:
 	print(s.recvfrom(65565))
